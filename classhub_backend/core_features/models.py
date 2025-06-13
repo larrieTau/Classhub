d8b5a15ee7e3bef535766ef8559919bd7fe41143
Tagoole Mugwa David <tagooledavid4@gmail.com>
2025-06-13 04:33:20 +0300

    Created model classes with out fields

--- classhub_backend/core_features/models.py
+++ classhub_backend/core_features/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
 # Create your models here.
+class Student_Report(models.Model):
+    pass
+
+class Financial_report(models.Model):
+    pass
+
+
+class SMS_Notificaton(models.Model):
+    pass
+
+class Subject(models.Model):
+    pass
+
+class Announcement(models.Model):
+    pass
+
+class Exam(models.Model):
+    pass
+
+class Health_Record(models.Model):
+    pass
+
+class Grade(models.Model):
+    pass
+
+class Leave_Application(models.Model):
+    pass
+
